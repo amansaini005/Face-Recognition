f003e78327d9bac588141cc314c977b24eb655e7
amansaini005 <110121353+amansaini005@users.noreply.github.com>
2022-09-07 23:15:57 +0530

    first

---
+++ layer.py
@@ -0,0 +1,12 @@
+# custom L1 Distance layer
+
+# import dependencies
+import tensorflow as tf
+from tensorflow.keras.layers import Layer
+
+class L1Dist(Layer):
+    def __init__(self, **kwargs):
+        super().__init__()
+
+        def call(self, input_embedding, validation_embedding):
+            return tf.math.abs(input_embedding - validation_embedding)
